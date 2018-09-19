2ce9d024f0354666b74e52126f811b67a0b1db84
Adams Shittu <shttdms@gmail.com>
2018-09-19 09:29:57 +0100

    trying to solve the issue of gh-pages not working

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
+//Nav bar mobile/tablet dropdown menu
+let navToggle = document.querySelector('header div i');
+let navBar = document.querySelector('nav');
+
+navToggle.addEventListener('click', () => {
+    if (window.matchMedia("(max-width: 823px)").matches) {
+        if (navBar.style.display === "none" || navBar.style.display == '') {
+         navBar.style.display = "block";
+         navBar.style.position = "absolute";
+     } else {
+        navBar.style.display = "none";
+       }
+    }  
+});
 
 
 //For Rendering the Admin Dashboard
 let loginpassword = document.querySelector("#loginpassword");
 let loginbutton = document.querySelector("#loginbutton");
 
-loginbutton.addEventListener("click", function(){
-	if(loginpassword.value === "admin"){
-		window.open("admin.html");
-	}
-});
-
-
+if(loginbutton){
+    loginbutton.addEventListener('click', () => {
+        if(loginpassword.value === "admin"){
+            window.open("../html/admin.html");
+        }
+    });
+}
