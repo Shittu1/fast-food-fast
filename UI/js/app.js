b2da96cf30b837a6382775f37db3192c11751694
Adams Shittu <shttdms@gmail.com>
2018-09-25 05:53:22 +0100

    remove all the nav links in the admin section except the home link

--- UI/js/app.js
+++ UI/js/app.js
@@ -23,5 +23,46 @@ if(loginbutton){
         if(loginpassword.value === "admin"){
             window.open("../admin/allproducts.html");
         }
+        if(loginpassword.value === "user"){
+            window.open("../loggedin/delicacies.html");
+        }
     });
 }
+
+
+
+//Incrementation of cart
+let cart =[];
+
+let addToCartButtons = document.querySelectorAll("a.addtocartbutton");
+let cartCount = document.querySelector("a.cartcount");
+let bracketleft = document.querySelector("span.bracketleft");
+let bracketright = document.querySelector("span.bracketright");
+
+let cartOperations = (function(){
+    return (
+
+            addToCartButtons.forEach(btn => btn.addEventListener("click", (event) => {
+            event.preventDefault();
+            cartCount.style.display = "inline";
+            bracketleft.style.display = "inline";
+            bracketright.style.display = "inline";
+            cartCount.textContent++;
+            let name = event.target.getAttribute('name');
+            let price = Number(event.target.getAttribute('price'));
+            let image = event.target.getAttribute('image');
+            let cartitems = {name: name, price: price, image: image, count: 1};
+
+            if(Object.keys(cart).indexOf(cart.name) == -1){
+                cart.push(cartitems);
+            }else if(Object.keys(cart).indexOf(cart.name) > -1){
+                cart.count++;
+            }
+            saveCart();
+            console.log(cart);
+
+            }))
+
+        );
+        
+})();
